21598ac2e1785d86aa236417abcb284df4346bc0
Lilian Etchepare <dev@lilian-etchepare.com>
2024-09-13 11:27:11 +0200

    exercise 3: render vms in table

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var axios = require("axios");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async function (req, res, next) {
+  try {
+    const response = await axios.get("http://localhost:3000/api/vms");
+    const vms = response.data;
+    res.render("index", { title: "Express", vms });
+  } catch (error) {
+    next(error);
+  }
 });
-
 module.exports = router;
